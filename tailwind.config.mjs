9b3f8adbcc949b72a17b76bc945f02b91297d3a5
Afra Anjum <lilithkard77@gmail.com>
2025-10-20 17:21:39 +0600

    changed typography

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,7 +10,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
-        serif: ["Lora", ...defaultTheme.fontFamily.serif],
+        mono: ["jetBrains Mono", ...defaultTheme.fontFamily.mono]
       },
     },
   },
