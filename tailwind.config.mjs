ea4597f4233570814ed14d3b5cbb0d4277e0efe4
Afra Anjum <lilithkard77@gmail.com>
2025-10-20 18:16:22 +0600

    added jetBrains Mono ttf and fixed typography

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,14 +3,13 @@ import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
-        mono: ["jetBrains Mono", ...defaultTheme.fontFamily.mono]
+        mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
+        jetMono: ["jetMono", ...defaultTheme.fontFamily.mono],
       },
     },
   },
